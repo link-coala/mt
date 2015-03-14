efc88ff0ad2e310370be8a2221b34cb334af650d
link-coala <mpj183@aol.com>
2015-03-14 16:31:59 +0000

    hide-check

--- simple-todos.js
+++ simple-todos.js
@@ -3,8 +3,19 @@ if (Meteor.isClient) {
 // This code only runs on the client
 Template.body.helpers({
 tasks: function () {
-// Show newest tasks first
+if (Session.get("hideCompleted")) {
+// If hide completed is checked, filter tasks
+return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+} else {
+// Otherwise, return all of the tasks
 return Tasks.find({}, {sort: {createdAt: -1}});
+}
+},
+hideCompleted: function () {
+return Session.get("hideCompleted");
+},
+incompleteCount: function () {
+return Tasks.find({checked: {$ne: true}}).count();
 }
 });
 Template.body.events({
@@ -19,6 +30,9 @@ createdAt: new Date() // current time
 event.target.text.value = "";
 // Prevent default form submit
 return false;
+},
+"change .hide-completed input": function (event) {
+Session.set("hideCompleted", event.target.checked);
 }
 });
 Template.task.events({
