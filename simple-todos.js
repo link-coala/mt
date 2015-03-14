b653a36f8a8ca1463f2bf6f956076afd4e179306
link-coala <mpj183@aol.com>
2015-03-14 13:33:35 +0000

    step 3

--- simple-todos.js
+++ simple-todos.js
@@ -1,11 +1,25 @@
-// simple-todos.js
 Tasks = new Mongo.Collection("tasks");
-
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
-      return Tasks.find({});
-    }
-  });
+// This code only runs on the client
+Template.body.helpers({
+tasks: function () {
+  
+// Show newest tasks first
+return Tasks.find({}, {sort: {createdAt: -1}});
+}
+});
+Template.body.events({
+"submit .new-task": function (event) {
+// This function is called when the new task form is submitted
+var text = event.target.text.value;
+Tasks.insert({
+text: text,
+createdAt: new Date() // current time
+});
+// Clear form
+event.target.text.value = "";
+// Prevent default form submit
+return false;
+}
+});
 }
